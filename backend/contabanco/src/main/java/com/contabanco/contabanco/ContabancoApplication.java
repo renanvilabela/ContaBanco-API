5c5bff500b78dab2f6aac06e55025af5f828ff41
renanvilabela <vilabelarenan@gmail.com>
2025-04-12 15:00:37 -0300

    criação de ambiente de execução, frontend e backend da API

---
+++ backend/contabanco/src/main/java/com/contabanco/contabanco/ContabancoApplication.java
@@ -0,0 +1,13 @@
+package com.contabanco.contabanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabancoApplication.class, args);
+	}
+
+}
---
+++ backend/contabanco/src/test/java/com/contabanco/contabanco/ContabancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contabanco.contabanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
